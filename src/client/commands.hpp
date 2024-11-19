c5ab72be58253a77c1c11280650ef6f61586cee2
Leticia <leticia.vong@tecnico.ulisboa.pt>
2024-11-19 18:15:45 +0000

    commands subclasses

--- src/client/commands.hpp
+++ src/client/commands.hpp
@@ -12,50 +12,112 @@
 
 
 class CommandHandler {
-    protected:
-    /**
-     * @brief Constructs a CommandHandler object with the specified properties.
-     *
-     * @param name The name of the command.
-     * @param args The args of the command.
-     * @param usage The description of the command.
-     */
-    
-        CommandHandler(const char* __name, const std::optional<const char*> __args,
-                        const char* __usage)
-            :   name{__name},
-                args{__args},
-                usage{__usage} {}
-                
-    public:
-        /**
-         * @brief Handles the command with the given arguments.
-         * should be implemented by derived classes
-        */
 
-        virtual void handle(std::string args, Player_Info &receiver) = 0;
+  public:
 
+      const char* name;                           // The name of the command
+      const std::optional<const char*> alias;     // The aliases of the command
+      const std::optional<const char*> args;      // The args of the command
+      const std::optional<const char*> usage;     // The description of the command
 
-        const char* name;                           // The name of the command
-        const std::optional<const char*> args;      // The args of the command
-        const std::optional<const char*> usage;     // The description of the command
+      /**
+       * @brief Handles the command with the given arguments.
+       * should be implemented by derived classes
+      */
+      virtual void handle(std::string args, Player_Info &receiver) = 0;
 
-        void readCommand(std::string command, Player_Info &state);
-    
+      // void readCommand(std::string command, Player_Info &state);
 
-
+  protected:
+  /**
+   * @brief Constructs a CommandHandler object with the specified properties.
+   *
+   * @param name The name of the command.
+   * @param alias The aliases of the command
+   * @param args The args of the command.
+   * @param usage The description of the command.
+   */
+  
+    CommandHandler(const char* __name, const std::optional<const char*> __alias,
+          const std::optional<const char*> __args, const char* __usage)
+        :   name{__name},
+            alias{__alias},
+            args{__args},
+            usage{__usage} {}
+              
 };
-
 
 
 class StartCommand : public CommandHandler {
-    void handle(std::string args);
+  void handle(std::string args, Player_Info &state);
 
-    public:
-    StartCommand() : CommandHandler("start", "PLID", "Start a new game") {}
+  public:
+    StartCommand() : CommandHandler("start", std::nullopt, "PLID max_playtime",
+                                    "Start a new game") {}
 };
 
 
+class TryCommand : public CommandHandler {
+  void handle(std::string args, Player_Info &state);
+
+  public:
+    TryCommand(): CommandHandler("try", std::nullopt, "C1 C2 C3 C4", 
+                                "Tries a combination of colors") {}
+};
+
+
+class ShowTrialsCommand : public CommandHandler {
+  void handle(std::string args, Player_Info& state);
+
+  public:
+    ShowTrialsCommand(): 
+        CommandHandler("show_trials", "st", std::nullopt,
+                      "Display previously made trials and respective results") {}
+};
+
+
+
+class ScoreboardCommand : public CommandHandler {
+  void handle(std::string args, Player_Info& state);
+
+  public:
+    ScoreboardCommand() : CommandHandler("scoreboard", "sb", std::nullopt, 
+                                        "Display the scoreboard") {}
+};
+
+
+
+class QuitCommand : public CommandHandler {
+  void handle(std::string args, Player_Info& state);
+
+  public:
+    QuitCommand()
+        : CommandHandler("quit", std::nullopt, std::nullopt, "Quit game") {}
+};
+
+
+
+class ExitCommand : public CommandHandler {
+  void handle(std::string args, Player_Info& state);
+
+ public:
+  ExitCommand()
+      : CommandHandler("exit", std::nullopt, std::nullopt, "Exit application") {
+  }
+};
+
+
+
+class DebugCommand : public CommandHandler {
+  void handle(std::string args, Player_Info& state);
+
+ public:
+  DebugCommand()
+      : CommandHandler("debug", std::nullopt, "PLID max_playtime C1 C2 C3 C4", 
+                      "Exit application") {
+  }
+
+};
 
 
 
@@ -76,6 +138,7 @@ class CommandException : public std::runtime_error {
         : std::runtime_error("ERROR: " + reason){};
 };
 
+
 /**
  * @brief Exception thrown when invalid arguments are provided for a command.
  */
@@ -90,6 +153,7 @@ class CommandArgumentException : public CommandException {
     CommandArgumentException(std::string usage)
         : CommandException("Invalid arguments.\nUsage: " + usage){};
 };
+
 
 /**
  * @brief Exception thrown when an unknown command is encountered.
