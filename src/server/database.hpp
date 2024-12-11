3ba4915f1b21a41b6609a19965fc264b16234e5c
Teresa <teresa.vargas.matos@tecnico.ulisboa.com>
2024-12-11 12:30:52 +0000

    DatabseManager prototype

--- src/server/database.hpp
+++ src/server/database.hpp
@@ -1,6 +1,9 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <string>
+#include <vector>
+#include <fstream>
 #include <iostream>
 
 /*A directoria GAMES contém um ficheiro por cada jogo em curso contendo o estado do jogo, e ainda as directorias de jogadores, contendo
@@ -13,5 +16,27 @@ directoria GAMES. O caminho para a localização dos ficheiros de jogos finaliza
 
 GAME (plid).txt:
 PPPPPP M CCCC T YYYY-MM-DD HH:MM:SS s*/
+
+class DatabaseManager
+{
+private:
+    std::string m_rootDir; // Root directory for all data files
+
+    // Helper methods
+    bool openFile(std::fstream &fileStream, const std::string &filePath, std::ios_base::openmode mode);
+    bool closeFile(std::fstream &fileStream);
+
+public:
+    DatabaseManager(/* args */);
+    ~DatabaseManager();
+
+    bool CreateDirectory(std::string path);
+    bool CreateFile(std::string path);
+    bool AppendToFile(std::string path, std::string content);
+};
+
+DatabaseManager::DatabaseManager(/* args */)
+{
+}
 
 #endif
